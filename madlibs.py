859a1ed6e0c8d7f7d2664730cab4b62b03bdfe59
techydev101 <82757569+techydev101@users.noreply.github.com>
2021-05-07 13:19:24 +0100

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+#madlibs program
+#user input
+    #adjectives
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+    #verbs
+verb1 = input("Verb: ")    
+verb2 = input("Verb: ")  
+verb3 = input("Verb: ")  
+
+#the concatenated string
+print(f"A {adj1} sea has a {adj2} pulse to it unmatched by any other part of nature. \n\
+It {verb1} its own sounds and kindles its own symphony.\n\
+If you find yourself visualising its {adj4} vastness,\n\
+its {adj3} surface and straining to {verb2} its metronomic wave music,\n\
+you may {verb3} why it's called the fisherman's friend.")
